db5380b090cea0eacad248ec8e19c598c0a84a36
PeterALX <quaypeter25@gmail.com>
2022-12-08 12:23:07 +0000

    add

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if roman_string == None or isinstance(roman_string, str) == False:
-        return 0
+        return (0)
     else:
         roman_ = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         num = 0
@@ -10,4 +10,4 @@ def roman_to_int(roman_string):
                 num += roman_[roman_string[i]] - 2 * roman_[roman_string[i - 1]]
             else:
                 num += roman_[roman_string[i]]
-        return(num)
+        return (num)
