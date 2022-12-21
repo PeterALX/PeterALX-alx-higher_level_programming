10d05af77eb6dfdbab9960df1507469f1be5e54d
PeterALX <quaypeter25@gmail.com>
2022-12-21 23:10:25 +0000

    add

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A class Square that defines a square"""
+
+
+class Square:
+    """Module of a square
+    Attributes:
+        __size (int): Describes the length of a square
+    """
+    def __init__(self, size=0):
+        """Initializes the square attributes
+        Args:
+            size (int): Describes the length of a square
+        Returns:
+            None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
