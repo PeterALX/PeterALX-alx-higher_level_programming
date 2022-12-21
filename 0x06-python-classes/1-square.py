425bcdd64655e3075760f32f15e05641791967bf
PeterALX <quaypeter25@gmail.com>
2022-12-21 23:07:21 +0000

    add

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A class Square that defines a square"""
+
+
+class Square:
+    """Module of a square
+    Attributes:
+        __size (int): Describes the length of a square
+    """
+    def __init__(self, size):
+        """Initializes the square attributes
+        Args:
+            size (int): Describes the length of a square
+        Returns:
+            None
+        """
+        self.__size = size
