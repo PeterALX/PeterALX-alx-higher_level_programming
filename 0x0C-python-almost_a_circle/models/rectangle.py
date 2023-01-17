6b501b7a31e41c159d8ef3ad56ce00139c80f561
PeterALX <quaypeter25@gmail.com>
2023-01-17 11:04:40 +0000

    second task almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -45,3 +45,27 @@ class Rectangle(Base):
             ValueError: if width is less than or equal to zero.
         """
         self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
