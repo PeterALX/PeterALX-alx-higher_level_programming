933c69af17dba4f5f04beda98b802d62c35391c6
PeterALX <quaypeter25@gmail.com>
2023-01-17 10:22:37 +0000

    first commit almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 """ class base"""
+
 
 class Base:
     """Class that defines properties of Base.
@@ -10,7 +10,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
