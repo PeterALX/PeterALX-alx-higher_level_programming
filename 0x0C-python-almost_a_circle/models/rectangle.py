36993719a5ea4fc5cfa68815cd3c9dedfca9cce9
PeterALX <quaypeter25@gmail.com>
2023-01-17 10:47:36 +0000

    second task almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,8 @@
-from models.base import Base
+#!/usr/bin/python3
 """class Rectangle"""
+
+from models.base import Base
+
 
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
@@ -12,6 +15,7 @@ class Rectangle(Base):
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, value):
         self.__width = value
