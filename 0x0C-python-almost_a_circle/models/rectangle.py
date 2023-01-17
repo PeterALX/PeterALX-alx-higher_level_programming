44e84968a743de27b777f3a307fe9bfb7690d64d
PeterALX <quaypeter25@gmail.com>
2023-01-17 10:49:59 +0000

    second task almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,6 +5,13 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """Class that defines properties of Rectangle.
+     Attributes:
+        width (int): width of rectangle.
+        height (int): height of rectangle.
+        x (int): x.
+        y (int): y.
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
         super(id)
         self.width = width
