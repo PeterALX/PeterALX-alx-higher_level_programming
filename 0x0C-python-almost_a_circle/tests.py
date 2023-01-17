6de8b92507b21a5c81ffc2f4f60ed22bade0f442
PeterALX <quaypeter25@gmail.com>
2023-01-17 11:32:11 +0000

    add'

--- 0x0C-python-almost_a_circle/tests.py
+++ 0x0C-python-almost_a_circle/tests.py
@@ -12,3 +12,18 @@ if __name__ == "__main__":
 
     r3 = Rectangle(10, 2, 0, 0, 12)
     print(r3.id)
+#!/usr/bin/python3
+""" 3-main """
+from models.rectangle import Rectangle
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(3, 2)
+    print(r1.area())
+
+    r2 = Rectangle(2, 10)
+    print(r2.area())
+
+    r3 = Rectangle(8, 7, 0, 0, 12)
+    print(r3.area())
+
