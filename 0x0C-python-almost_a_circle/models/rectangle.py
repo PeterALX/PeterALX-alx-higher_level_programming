20ec8268febe5395bbd1127b1ed5de3b0ee4fc59
PeterALX <quaypeter25@gmail.com>
2023-01-18 09:13:28 +0000

    add

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,13 +101,16 @@ class Rectangle(Base):
             print(" " * self.__x + "#" * self.__width)
 
     def update(self, *args):
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.__width = args[1]
-        if len(args) >= 3:
-            self.__height = args[2]
-        if len(args) >= 4:
-            self.__x = args[3]
-        if len(args) >= 5:
-            self.__y = args[4]
+        list_atrr = ["id", "width", "height", "x", "y"]
+        for i in range(len(args)):
+            setattr(self, list_atrr[i], args[i])
+        # if len(args) >= 1:
+        #     self.id = args[0]
+        # if len(args) >= 2:
+        #     self.__width = args[1]
+        # if len(args) >= 3:
+        #     self.__height = args[2]
+        # if len(args) >= 4:
+        #     self.__x = args[3]
+        # if len(args) >= 5:
+        #     self.__y = args[4]
