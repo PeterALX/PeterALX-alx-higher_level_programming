81dbfb26a66f2026f0a7b0b4766c1f981a723904
PeterALX <quaypeter25@gmail.com>
2023-01-18 08:28:19 +0000

    add

--- 0x0C-python-almost_a_circle/tests.py
+++ 0x0C-python-almost_a_circle/tests.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-""" 5-main """
+""" 6-main """
 from models.rectangle import Rectangle
 
 if __name__ == "__main__":
 
-    r1 = Rectangle(4, 6, 2, 1, 12)
+    r1 = Rectangle(2, 3, 2, 2)
     print(r1)
+    r1.display()
 
-    r2 = Rectangle(5, 5, 1)
+    print("---")
+
+    r2 = Rectangle(3, 2, 1, 0)
     print(r2)
+    r2.display()
