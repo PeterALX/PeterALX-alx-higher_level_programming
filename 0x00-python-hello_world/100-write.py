c1b00826ef36884e38505663e3b11a2078c23246
PeterALX <quaypeter25@gmail.com>
2023-10-02 13:16:15 +0000

    some more changes

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
+from sys import stderr
 
-import sys
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-sys.exit(1)
+stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+exit(1)
--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(f"{str * 3}")
-print(f"{str[:9]}")
-
-# Print last 6 characters in string
-# print(f"{str[-6:]}")
+print(3 * str)
+print(str[0:9])
