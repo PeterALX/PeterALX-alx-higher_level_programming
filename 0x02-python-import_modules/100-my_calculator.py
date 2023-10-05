9d459c3503ded7e62273d3a75debbd1e44b98db2
PeterALX <quaypeter25@gmail.com>
2023-10-05 16:56:26 +0000

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,4 +3,24 @@
 import sys
 from calculator_1 import *
 
-print(len(sys.argv))
+if len(sys.argv) != 4:
+    print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+    sys.exit(1)
+
+
+op = sys.argv[2]
+a = int(sys.argv[1])
+b = int(sys.argv[3])
+
+if op ==  '+':
+    print(f"{a} {op} {b} = {add(a,b)}")
+elif op ==  '-':
+    print(f"{a} {op} {b} = {sub(a,b)}")
+elif op ==  '*':
+    print(f"{a} {op} {b} = {mul(a,b)}")
+elif op ==  '/':
+    print(f"{a} {op} {b} = {div(a,b)}")
+else:
+    print('Unknown operator. Available operators: +, -, * and /')
+    sys.exit(1)
+
