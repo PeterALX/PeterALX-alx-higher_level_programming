7e462d07b9cfdc58bf095bb9676fc98c0110838e
PeterALX <quaypeter25@gmail.com>
2024-02-08 13:04:50 +0000

    done 100

--- 0x06-python-classes/main.py
+++ 0x06-python-classes/main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-Square = __import__('6-square').Square
+SinglyLinkedList = __import__('100-singly_linked_list').SinglyLinkedList
 
-my_square_3 = Square(5, (3, 2))
-my_square_3.my_print()
-print("--")
-my_square_3.my_printt()
+sll = SinglyLinkedList()
+# print(sll)
+sll.sorted_insert(2)
+sll.sorted_insert(5)
+sll.sorted_insert(3)
+sll.sorted_insert(10)
+sll.sorted_insert(1)
+sll.sorted_insert(-4)
+sll.sorted_insert(-3)
+sll.sorted_insert(4)
+sll.sorted_insert(5)
+sll.sorted_insert(12)
+sll.sorted_insert(3)
