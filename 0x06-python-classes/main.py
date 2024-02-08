d90a53357365bb0269d2c04aba8e028a216c1ef4
PeterALX <quaypeter25@gmail.com>
2024-02-08 13:18:58 +0000

    fix 100

--- 0x06-python-classes/main.py
+++ 0x06-python-classes/main.py
@@ -2,7 +2,6 @@
 SinglyLinkedList = __import__('100-singly_linked_list').SinglyLinkedList
 
 sll = SinglyLinkedList()
-# print(sll)
 sll.sorted_insert(2)
 sll.sorted_insert(5)
 sll.sorted_insert(3)
@@ -14,3 +13,4 @@ sll.sorted_insert(4)
 sll.sorted_insert(5)
 sll.sorted_insert(12)
 sll.sorted_insert(3)
+print(sll)
