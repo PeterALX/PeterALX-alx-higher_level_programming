1b6d6c68d3339e91e4e3ab04df788b96c8566aaa
PeterALX <quaypeter25@gmail.com>
2024-02-14 22:18:50 +0000

    hmmm

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""magic_string in the houuuusee"""
+
+
+def magic_string(a=[]):
+    a += ["BestSchool"]
+    return (", ".join(a))
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""locked class"""
+
+
+class LockedClass:
+    """The module prevents the user from dynamically creating new instance
+    attributes,except if the new instance attribute is called first_name"""
+    __slots__ = ["first_name"]
