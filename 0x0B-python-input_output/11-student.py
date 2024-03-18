aa6699c6d4a9ad43792d979b2ea1491d2db418df
PeterALX <quaypeter25@gmail.com>
2024-03-18 18:29:08 +0000

    done 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""class student be up in here"""
+
+
+class Student:
+    """the student class be I and I rastafari"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        return {k: v for (k, v) in self.__dict__.items() if k in attrs}
+
+    def reload_from_json(self, json):
+        for key in json:
+            self.__dict__[key] = json[key]
