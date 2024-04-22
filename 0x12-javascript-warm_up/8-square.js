64bb97e633bca1926e3ce89eb7ddf450b6a0aee2
PeterALX <quaypeter25@gmail.com>
2024-04-22 18:19:21 +0000

    done javascript warmup

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+'use strict';
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let count = 0; count < process.argv[2]; count++) {
+    console.log('X'.repeat(process.argv[2]));
+  }
+}
