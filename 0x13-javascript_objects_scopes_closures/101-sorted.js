9a0b3a256309de80d94f2ae98b1d937596d47d3e
PeterALX <quaypeter25@gmail.com>
2024-04-24 12:49:23 +0000

    fix 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const dict = require('./dict.js').dict;
+const dict = require('./101-data.js').dict;
 const d = {};
 for (const [k, v] of Object.entries(dict)) {
   if (d[v] === undefined) {
