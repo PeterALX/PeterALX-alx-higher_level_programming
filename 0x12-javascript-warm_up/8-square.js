05614b195fd2bd9c0322b59593d0d8f74deb63be
PeterALX <quaypeter25@gmail.com>
2024-05-13 12:12:29 +0000

    deleted higher level programming tasks for deferment

--- 0x12-javascript-warm_up/8-square.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-'use strict';
-if (isNaN(process.argv[2])) {
-  console.log('Missing size');
-} else {
-  for (let count = 0; count < process.argv[2]; count++) {
-    console.log('X'.repeat(process.argv[2]));
-  }
-}
