56869283854c1c9f3caec9e521eb1617e1f2c5c3
PeterALX <quaypeter25@gmail.com>
2024-07-30 10:22:19 +0000

    minor change

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,7 +6,7 @@ request(process.argv[2], (err, response, body) => {
   if (err) {
     console.log(err);
   } else {
-    fs.writeFile(process.argv[3], body, 'utf8', (err, data) => {
+    fs.writeFile(process.argv[3], body, 'utf8', (err) => {
       if (err) {
         console.log(err);
       }
