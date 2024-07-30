505845eebd76a5155e3094958559c07759bfa833
PeterALX <quaypeter25@gmail.com>
2024-07-30 10:19:36 +0000

    jscrape done 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+request(process.argv[2], (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
